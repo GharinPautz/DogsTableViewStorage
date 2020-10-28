346b2575f2a6cca393e7290984d0a17cdf4ab14c
Gharin Pautz <gharinpautz@gmail.com>
2020-10-28 13:12:08 -0700

    after 10/23 recording

--- DogStorage/DogTableViewController.swift
+++ DogStorage/DogTableViewController.swift
@@ -17,7 +17,7 @@ import UIKit
 
 // This design utilizes method 1
 
-class DogTableViewController: UIViewController {
+class DogTableViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     var dogs = [Dog]()
     
@@ -35,6 +35,34 @@ class DogTableViewController: UIViewController {
         dogs.append(Dog(name: "Daphne", breed: "Golden Doodle"))
     }
 
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // table view is asking data source this question:
+        //      How many rows do I put in this section?
+        
+        // We are only going to have one section
+        if section == 0 {
+            return dogs.count
+        }
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // table view is asking data source this question:
+        //      What cell should I put at this indexPath?
+        // indexPath has 2 properties
+        //      sectionNumber (we will ignore)
+        //      rowNumber (corresponds to array index number)
+        let row = indexPath.row
+        let dog = dogs[row]
+        
+        // we want a cell that displays this dog's name and breed
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DogCell", for: indexPath) as! DogTableViewCell
+        
+        // why a reusable cell?
+        //      If dogs array had 10000 dogs, iOS creates a pool of reusable cells to display our rows with a nice animation
+        
+        cell.update(with: dog)
+        return cell
+    }
 }
 
